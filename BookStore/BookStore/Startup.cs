fae07707036ce317d376d54b7b6e00b892049cf7
Alex <al6481078@gmail.com>
2021-11-18 05:47:00 +0300

    Дефолтная настройка mvc

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(services => services.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,8 @@ namespace BookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
+            
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();           }
     }
 }
